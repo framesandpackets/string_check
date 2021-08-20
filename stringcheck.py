2431caa7651870b19be2a68748aba349497dad86
framesandpackets <michaelhanna272@hotmail.com>
2021-08-20 17:27:47 +0100

    fixed regex pattern

--- stringcheck.py
+++ stringcheck.py
@@ -10,25 +10,28 @@ string = input("\n Enter string you would like to check: ")   #calls for user in
 print ("-------------------------------------------------------------")
 
 def string_checker():		#string checker function
-	quote_match = re.findall('"([^"]*)"',string)	#regex pattern for double quotes and text between quotes
+	quote_match = re.match(r'"\w+"',string)	#regex pattern for double quotes and text between quotes
 	is_match = bool(quote_match)		#returns boolean value for regex call
 	num_range = re.match(r'[0-9][0-2]', string)      #regex match pattern for intergers in range 1-12 in string using groupings
 	bool_num = bool(num_range)
-	pattern = re.compile(r'[.]$')		#compiling/loading regular expression for fullstop at end of string
-	matched_fullstop = pattern.finditer(string)		#findering iterations of regex pattern above
-	bool_fullstop = bool(matched_fullstop)
+#	pattern = re.compile(r'[.]$')		#compiling/loading regular expression for fullstop at end of string
+#	matched_fullstop = pattern.finditer(string)		#findering iterations of regex pattern above
+#	bool_fullstop = bool(matched_fullstop)
 	if string[0].isupper(): 		#checking string for capital at start
 		print("Test Passed: Captial letter used at start of string")
 	else:
 		print("Test Failed: Captial letter not used at start of string")
+
 	if string[-1] == ".":
 		print("\nTest Passed: You have used a fullstop at the end of string")
 	else:
 		print("\nTest Failed: You string does end with a fullstop")
+
 	if is_match == True:
-		print("\nYour string uses double quotation marks")
+		print("\nTest Passed: You have used double quotes in your string")
 	else:
-		print("\nYour string is not using double quotation marks")
+		print("\nTest Failed: You have not used double quotes in your string")
+
 	if bool_num == True:
 		print("\nTest Failed: You have numbers below 13 that are not spelled out")
 	else:
