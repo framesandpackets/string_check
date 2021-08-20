9aea993505ac636b3c61166c4baec77e92198b21
framesandpackets <michaelhanna272@hotmail.com>
2021-08-20 12:10:55 +0100

    cleaning/modularizing code

---
+++ header.py
@@ -0,0 +1,15 @@
+print("""\n                [STRING CHECK APPLICATION]
+
+How to use: Type valid/invalid string when prompted in terminal.
+
+Checks to be done:
+
+· String starts with a capital letter
+
+· String has an even number of quotation marks
+
+· String ends with a period character “."
+
+· String has no period characters other than the last character
+
+· Numbers below 13 are spelled out (”one”, “two”, "three”, etc…) \n\n""")
--- stringcheck.py
+++ stringcheck.py
@@ -1,22 +1,9 @@
 #!/usr/bin/env python
 
 import re		#importing regex module
+import header as welcome		#import module from local dir
 
-print("""\n                [STRING CHECK APPLICATION]
-
-How to use: Type valid/invalid string when prompted in terminal.
-
-Checks to be done:
-
-· String starts with a capital letter
-
-· String has an even number of quotation marks
-
-· String ends with a period character “."
-
-· String has no period characters other than the last character
-
-· Numbers below 13 are spelled out (”one”, “two”, "three”, etc…) \n\n""")
+welcome()		#calling welcome/landing module
 
 string = input("\n Enter string you would like to check: ")   #calls for user input of string
 
